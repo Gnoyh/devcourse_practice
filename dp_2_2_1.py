66b0ab6598fa5c27d3fc4075c2d0ae17db1e4ed8
Gnoyh <4ksenhs@gmail.com>
2024-03-26 12:40:33 +0900

    연결 리스트 순회

---
+++ dp_2_2_1.py
@@ -0,0 +1,12 @@
+# 연결 리스트 순회
+
+def traverse(self):
+        ll = []
+        curr = self.head
+        
+        while curr != None:
+            ll.append(curr.data)
+            
+            curr = curr.next
+            
+        return ll
