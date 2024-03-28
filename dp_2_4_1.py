ec33a06b620010350f3287ee26e52406d967eb4f
Gnoyh <4ksenhs@gmail.com>
2024-03-28 12:17:06 +0900

    완주하지 못한 선수

---
+++ dp_2_4_1.py
@@ -0,0 +1,15 @@
+# 완주하지 못한 선수
+
+def solution(participant, completion):
+    result_dict = {}
+    
+    for i in participant:
+        result_dict[i] = result_dict.get(i, 0) + 1
+    
+    for i in completion:
+        result_dict[i] -= 1
+        
+        if result_dict[i] == 0:
+            del result_dict[i]
+            
+    return list(result_dict.keys())[0]
