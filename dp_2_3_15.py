842996fd254bf3ddb96c694d7abbeb125da54a11
Gnoyh <4ksenhs@gmail.com>
2024-03-30 20:41:18 +0900

    최솟값 만들기

---
+++ dp_2_3_15.py
@@ -0,0 +1,12 @@
+# 최솟값 만들기
+
+def solution(A,B):
+    result = 0
+    
+    A.sort()
+    B.sort(reverse = True)
+    
+    for i in range(len(A)):
+        result += A[i] * B[i]
+        
+    return result
