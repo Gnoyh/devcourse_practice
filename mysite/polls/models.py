25c9e7275f541b153a99fbfc5ff216b7b2132874
Gnoyh <4ksenhs@gmail.com>
2024-04-08 16:25:36 +0900

    Shell 레코드 생성

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Question(models.Model):
     question = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(auto_now_add=True)
     # score = models.FloatField(default=0.0)
     # is_something_wrong = models.BooleanField(default=False)
     # json_field = models.JSONField(default=dict)
