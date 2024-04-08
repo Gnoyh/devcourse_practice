598fba4640742a873e5da3f681c5577122cdfd38
Gnoyh <4ksenhs@gmail.com>
2024-04-08 15:28:56 +0900

    Admin 모델 등록

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,6 +5,12 @@ from django.db import models
 class Question(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    # score = models.FloatField(default=0.0)
+    # is_something_wrong = models.BooleanField(default=False)
+    # json_field = models.JSONField(default=dict)
+
+    def __str__(self):
+        return f'제목: {self.question}, 날짜: {self.pub_date}'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
