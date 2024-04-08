db19b868d0684114768e8941aa05a58fc8ce3622
Gnoyh <4ksenhs@gmail.com>
2024-04-08 17:34:22 +0900

    모델 메소드

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 
@@ -9,8 +11,16 @@ class Question(models.Model):
     # is_something_wrong = models.BooleanField(default=False)
     # json_field = models.JSONField(default=dict)
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
     def __str__(self):
-        return f'제목: {self.question}, 날짜: {self.pub_date}'
+        if self.was_published_recently():
+            new_badge = "(NEW)"
+        else:
+            new_badge = ""
+
+        return f'{new_badge} 제목: {self.question}, 날짜: {self.pub_date}'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
