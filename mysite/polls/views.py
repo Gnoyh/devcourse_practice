db930749eb73dcc6670a5f2a325ad6c9936d0e40
Gnoyh <4ksenhs@gmail.com>
2024-04-09 14:59:01 +0900

    404 에러

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from .models import *
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[: 5]
@@ -9,7 +9,7 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    question = Question.objects.get(pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
 
     return render(request, 'polls/detail.html', {'question': question})
 
