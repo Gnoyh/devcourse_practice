e3696faf4257b66be792858eec0df78246e62e4f
Gnoyh <4ksenhs@gmail.com>
2024-04-09 15:29:55 +0900

    동시입력

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import *
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.db.models import F
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[: 5]
@@ -22,7 +23,7 @@ def vote(request, question_id):
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {'question': question, 'error_message': f'선택이 없습니다, id={request.POST['choice']}'})
     else:
-        selected_choice.votes += 1
+        selected_choice.votes = F('votes') + 1
         selected_choice.save()
 
         return HttpResponseRedirect(reverse('polls:index'))
