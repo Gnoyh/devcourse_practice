f5017a427fa9ba40ed4dc91e3aae29553b896af1
Gnoyh <4ksenhs@gmail.com>
2024-04-09 16:23:02 +0900

    Admin 커스터마이징

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,5 +3,18 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(Question)
 admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('질문 섹션', {'fields': ['question']}),
+        ('생성일', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    readonly_fields = ['pub_date']
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
