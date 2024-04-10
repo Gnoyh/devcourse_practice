212423278c05b048ed6696930591be7f048b0cc5
Gnoyh <4ksenhs@gmail.com>
2024-04-10 22:19:21 +0900

    POST

--- mysite/polls_api/views.py
+++ mysite/polls_api/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from polls.models import Question
 from polls_api.serializers import QuestionSerializer
 
 # Create your views here.
 
-@api_view()
+@api_view(['GET', 'POST'])
 def question_list(request):
-    questions = Question.objects.all()
-    serializer = QuestionSerializer(questions, many=True)
+    if request.method == 'GET':
+        questions = Question.objects.all()
+        serializer = QuestionSerializer(questions, many=True)
 
-    return Response(serializer.data)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = QuestionSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
