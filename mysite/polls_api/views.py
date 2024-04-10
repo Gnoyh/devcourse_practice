67aeefea717cc16e2b487d564d7d06ec99ef978e
Gnoyh <4ksenhs@gmail.com>
2024-04-10 18:18:48 +0900

    GET

--- mysite/polls_api/views.py
+++ mysite/polls_api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from polls.models import Question
+from polls_api.serializers import QuestionSerializer
 
 # Create your views here.
+
+@api_view()
+def question_list(request):
+    questions = Question.objects.all()
+    serializer = QuestionSerializer(questions, many=True)
+
+    return Response(serializer.data)
