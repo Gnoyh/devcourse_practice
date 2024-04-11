b359ca25148287da01501e81cfd3ddd882800670
Gnoyh <4ksenhs@gmail.com>
2024-04-11 17:42:21 +0900

    User 추가 - Form

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import *
 from django.shortcuts import render, get_object_or_404
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.db.models import F
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[: 5]
@@ -32,3 +34,8 @@ def result(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
 
     return render(request, 'polls/result.html', {'question': question})
+
+class SignupView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('user-list')
+    template_name = 'registration/signup.html'
