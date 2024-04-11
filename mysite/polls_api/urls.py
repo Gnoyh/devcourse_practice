ee6f2f9294c035cfc1b253f49cad18a05d4541f4
Gnoyh <4ksenhs@gmail.com>
2024-04-11 17:29:16 +0900

    User 관리

--- mysite/polls_api/urls.py
+++ mysite/polls_api/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('question/', QuestionList.as_view(), name='question-list'),
     path('question/<int:pk>/', QuestionDetail.as_view(), name='question_detail'),
+    path('users/', UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
 ]
--- mysite/polls_api/views.py
+++ mysite/polls_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from polls.models import Question
-from polls_api.serializers import QuestionSerializer
+from polls_api.serializers import QuestionSerializer, UserSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -11,3 +12,11 @@ class QuestionList(generics.ListCreateAPIView):
 class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
