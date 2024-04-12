b58f5a6ced8fe4eeeba4ef485e44153ef4a1499c
Gnoyh <4ksenhs@gmail.com>
2024-04-12 15:52:38 +0900

    Test

--- mysite/polls_api/tests.py
+++ mysite/polls_api/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from polls_api.serializers import *
 
 # Create your tests here.
+
+class QuestionSerializerTestCase(TestCase):
+    def test_with_vaild_data(self):
+        serializer = QuestionSerializer(data={'question': 'abc'})
+        self.assertEqual(serializer.is_valid(), True)
+        new_question = serializer.save()
+        self.assertIsNotNone(new_question.id)
+
+    def test_with_invalid_data(self):
+        serializer = QuestionSerializer(data={'question': ''})
+        self.assertEqual(serializer.is_valid(), False)
